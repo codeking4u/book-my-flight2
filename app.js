cf3c60aecfce342a8f43e944a23b88b42500e901
amar.singh <amar.singh@kuehne-nagel.com>
2024-08-31 16:00:28 +0300

    route init setup

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import router from './routes/routes.js';
 
 const app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => console.log('Server started'));
+app.use("/", router)
--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,10 @@
 import express from 'express'
 
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+    res.send('hello from routes')
+})
+
+
+export default router;
